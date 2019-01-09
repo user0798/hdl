3670228e9ae9b5a730000051e1f24a1f343fd071
Istvan Csomortani <istvan.csomortani@analog.com>
2019-01-09 14:19:35 +0000

    ad40xx: Fix the configuration of the up scaler
    The device provide data in twos complement format. The up scaler needs
    to support sign extension, in order to preserve the data format.

--- projects/ad40xx_fmc/common/ad40xx_bd.tcl
+++ projects/ad40xx_fmc/common/ad40xx_bd.tcl
@@ -53,9 +53,9 @@ current_bd_instance /spi_ad40xx
     ad_ip_parameter axis_upscaler CONFIG.NUM_OF_CHANNELS 1
     ad_ip_parameter axis_upscaler CONFIG.DATA_WIDTH $adc_resolution
     ad_ip_parameter axis_upscaler CONFIG.UDATA_WIDTH 32
-    ad_connect axis_upscaler/dfmt_enable GND
+    ad_connect axis_upscaler/dfmt_enable VCC
     ad_connect axis_upscaler/dfmt_type GND
-    ad_connect axis_upscaler/dfmt_se GND
+    ad_connect axis_upscaler/dfmt_se VCC
   }
 
   ad_connect axi/spi_engine_offload_ctrl0 offload/spi_engine_offload_ctrl
